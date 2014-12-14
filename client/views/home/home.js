8f5e5cc34e4989398c26f46a56d3a1f58e04167b
Sam Corcos <samcorcos@gmail.com>
2014-12-14 06:14:33 -0500

    added stats to homepage

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,5 +1,16 @@
 Template.home.rendered = function() {
+  Session.set("investedCapital", 812344);
+};
 
+Number.prototype.formatMoney = function(c, d, t){
+  var n = this,
+  c = isNaN(c = Math.abs(c)) ? 2 : c,
+  d = d == undefined ? "." : d,
+  t = t == undefined ? "," : t,
+  s = n < 0 ? "-" : "",
+  i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
+  j = (j = i.length) > 3 ? j % 3 : 0;
+  return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
 };
 
 Template.home.events({
@@ -7,5 +18,12 @@ Template.home.events({
 });
 
 Template.home.helpers({
-
+  investedCapital: function() {
+    var capital = Number(Session.get("investedCapital"))
+    return capital.formatMoney(0);
+  }
 });
+
+Meteor.setInterval(function() {
+  Session.set("investedCapital", Session.get("investedCapital")+Math.random()*10)
+}, 100)
