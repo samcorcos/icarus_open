95f45a9e3080b8650ae5c2c8a801a9bfadb3a055
Sam Corcos <samcorcos@gmail.com>
2014-12-16 10:53:26 -0500

    adding image carousel to single property page

--- client/views/single_property/single_property.js
+++ client/views/single_property/single_property.js
@@ -1,14 +1,24 @@
-Meteor.subscribe("images");
-
 Template.singleProperty.rendered = function() {
-
+  GoogleMaps.init(
+    {
+      'sensor': true, //optional
+      // 'key': 'AIzaSyCV8bo7e52UYRmmjqUoTGUfkvqR4Zq6rnE', //optional
+      'language': 'en' //optional
+    },
+    function(){
+      var mapOptions = {
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+      map.setCenter(new google.maps.LatLng( 41.870302, -87.764084 )); // This is going to have to be a function based on some calculation that gets the lat and long... good luck! ...Or just https://developers.google.com/maps/documentation/geocoding/
+    }
+  );
 };
 
 Template.singleProperty.helpers({
 
 });
-
-console.log()
 
 Template.singleProperty.events({
   'click #add-property-image': function(e,t) {
@@ -20,18 +30,15 @@ Template.singleProperty.events({
   }
 });
 
-GoogleMaps.init(
-{
-  'sensor': true, //optional
-  // 'key': 'AIzaSyCV8bo7e52UYRmmjqUoTGUfkvqR4Zq6rnE', //optional
-  'language': 'en' //optional
-},
-function(){
-  var mapOptions = {
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  map.setCenter(new google.maps.LatLng( 41.870302, -87.764084 )); // This is going to have to be a function based on some calculation that gets the lat and long... good luck! ...Or just https://developers.google.com/maps/documentation/geocoding/
+
+
+Template.singlePropertyImageCarousel.rendered = function() {
+  $('#carousel').slick({
+    dots: true,
+    arrows: true
+  });
 }
-);
+
+
+
+Meteor.subscribe("images");
