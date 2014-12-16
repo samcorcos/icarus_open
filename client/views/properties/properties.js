92568e1b54115c094125320b8fa2a67de1380ac9
Sam Corcos <samcorcos@gmail.com>
2014-12-16 14:23:50 -0500

    adding ranges to add property

--- client/views/properties/properties.js
+++ client/views/properties/properties.js
@@ -46,6 +46,81 @@ Template.newPropertyForm.helpers({
 });
 
 
+Template.newPropertyForm.rendered = function() {
+  var range_input = $('input[type=range]');
+  var range_mousedown = false;
+
+  range_input.each(function () {
+    var thumb = $('<span class="thumb"><span class="value"></span></span>');
+    $(this).after(thumb);
+  });
+
+  var range_wrapper = $('.range-field');
+
+  range_wrapper.on("mousedown", function(e) {
+    range_mousedown = true;
+    $(this).addClass('active');
+
+    var thumb = $(this).children('.thumb');
+    if (!thumb.hasClass('active')) {
+      thumb.velocity({ height: "30px", width: "30px", top: "-20px", marginLeft: "-15px"}, { duration: 300, easing: 'easeOutExpo' });
+    }
+    var left = e.pageX - $(this).offset().left;
+    var width = $(this).outerWidth();
+
+    if (left < 0) {
+      left = 0;
+    }
+    else if (left > width) {
+      left = width;
+    }
+    thumb.addClass('active').css('left', left);
+    thumb.find('.value').html($(this).children('input[type=range]').val());
+
+  });
+  range_wrapper.on("mouseup", function() {
+    range_mousedown = false;
+    $(this).removeClass('active');
+  });
+
+  range_wrapper.on("mousemove", function(e) {
+
+    var thumb = $(this).children('.thumb');
+    if (range_mousedown) {
+      if (!thumb.hasClass('active')) {
+        thumb.velocity({ height: "30px", width: "30px", top: "-20px", marginLeft: "-15px"}, { duration: 300, easing: 'easeOutExpo' });
+      }
+      var left = e.pageX - $(this).offset().left;
+      var width = $(this).outerWidth();
+
+      if (left < 0) {
+        left = 0;
+      }
+      else if (left > width) {
+        left = width;
+      }
+      thumb.addClass('active').css('left', left);
+      thumb.find('.value').html($(this).children('input[type=range]').val());
+    }
+
+  });
+  range_wrapper.on("mouseout", function() {
+    if (!range_mousedown) {
+
+      var thumb = $(this).children('.thumb');
+
+      if (thumb.hasClass('active')) {
+        thumb.velocity({ height: "0", width: "0", top: "10px", marginLeft: "-6px"}, { duration: 100 });
+      }
+      thumb.removeClass('active');
+
+    }
+
+
+  });
+  
+};
+
 // "change input#supplied-armor": function (events, template) {
 //   Battles.update(
 //     this._id,
