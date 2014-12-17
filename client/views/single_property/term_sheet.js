6ec2417278835f14382a7c798ed9dc44cb56f9b4
Sam Corcos <samcorcos@gmail.com>
2014-12-17 17:49:35 -0500

    term sheet is showing up when there is an existing termsheet

--- client/views/single_property/term_sheet.js
+++ client/views/single_property/term_sheet.js
@@ -2,18 +2,41 @@ Meteor.subscribe("termSheet");
 
 Template.singlePropertyTermSheet.rendered = function() {
 
+  var currentPropertyId = window.location.href.match(/(\/properties\/)(\w+)/)[2];
+  if (TermSheet.find({propertyId: currentPropertyId}).fetch()[0] !== undefined ) {
+    Session.set("termSheetActive", false);
+  } else { Session.set("termSheetActive", true); }
+
+  // Session.set("termSheetActive", function() {
+  //   return true;
+  //   // var currentPropertyId = window.location.href.match(/(\/properties\/)(\w+)/)[2];
+  //   // if (TermSheet.find({propertyId: currentPropertyId}).fetch() == []) {
+  //   //   return true;
+  //   // } else { return false; }
+  // })
+
 };
 
 Template.singlePropertyTermSheet.helpers({
   totalPrice: function() {
-    console.log(TermSheet.find().fetch())
-    return TermSheet.find().fetch()[0].totalPrice;
+    // if (TermSheet.find().fetch()[0].totalPrice != undefined) {
+    //   return TermSheet.find().fetch()[0].totalPrice.formatMoney(0);
+    // } else { var temp = 0; return temp.formatMoney(); }
+    // console.log(TermSheet.find().fetch()[0].totalPrice)
+    return 0;
+  },
+  termSheetActive: function() {
+    return Session.get("termSheetActive");
   }
 });
 
 Template.singlePropertyTermSheet.events({
   'click .term-sheet-item': function(e,t) {
     alert(e.currentTarget.value);
+  },
+  'click #add-term-sheet-button': function(e,t) {
+    $(".flex-white-div-term-sheet").toggleClass("add-flex-div-show")
+    $(".red-break-term-sheet").toggleClass("add-red-break-show")
   }
 });
 
