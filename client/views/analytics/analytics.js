83b0a9863915410a44361bf3fe2006bf4ed50bb7
Sam Corcos <samcorcos@gmail.com>
2014-12-18 17:24:07 -0600

    adding http

--- client/views/analytics/analytics.js
+++ client/views/analytics/analytics.js
@@ -12,3 +12,9 @@ Template.analytics.helpers({
 Template.analytics.events({
 
 });
+
+// zpid for place in chicago
+// 63706372
+
+// Below is a sample call to the API for zpid 48749425:
+// http://www.zillow.com/webservice/GetZestimate.htm?zws-id=<ZWSID>&zpid=48749425
