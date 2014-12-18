d909204d3960ffdf945df33bb9a14bfabdc4fc22
Sam Corcos <samcorcos@gmail.com>
2014-12-18 14:27:13 -0500

    minor fixes, add logic for equity investor

---
+++ client/views/single_property/equity_investor.js
@@ -0,0 +1,38 @@
+Template.equityInvestorModal.helpers({
+  
+});
+
+
+
+
+///////Consider a fade and a tab between "Analysis" and "Returns"
+
+// Total Price
+// Down Payment Percentage
+// Down Payment Amt
+// Closing Costs + Repairs Estimate
+// TOTAL INVESTMENT
+// Monthly Mortgage Expense
+// Taxes
+// Monthly Tax Average
+// HOA for condo
+// Insurance Approximation
+// Operating Expenses
+// RESERVE 5%
+// Monthly Cost of Ownership
+// Rent Price Full
+// CASHFLOW RENTED
+// CASHFLOW OCCUPIED
+// CASHFLOW UNOCCUPIED
+// AFTER TAX WITH RENTERS
+// AFTER TAX UN-OCCUPIED
+// FREE CASH FLOW (w/o prp tax)
+
+// Annual Revenue
+// Annual Cost
+// Annual Profit
+// Annual Profit After Tax
+// Annual ROI Before Tax
+// Annual ROI
+// OPERATING EXPENSE
+// NET OPERATING INCOME
