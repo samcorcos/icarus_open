91c51dbcea8dc07420b618654cb184374a540635
Sam Corcos <samcorcos@gmail.com>
2014-12-18 04:41:30 -0500

    calculator buttons and inputs working

--- client/views/calculator/calculator.js
+++ client/views/calculator/calculator.js
@@ -1,7 +1,5 @@
 Calculator = new Mongo.Collection(null);
 
-
-
 Template.calculator.created = function() {
   Calculator.insert({ purchasePrice: 0, closingRepair: 0, monthlyIncome: 0, capRate: 0 });
 };
@@ -9,20 +7,69 @@ Template.calculator.created = function() {
 Template.calculator.helpers({
   calculator: function() {
     return Calculator.find({});
+  },
+
+  editingTotalPriceCalc: function() {
+    return (Session.get("editingTotalPriceCalc"));
+  },
+  editingClosingRepairCalc: function() {
+    return (Session.get("editingClosingRepairCalc"));
+  },
+  editingMonthlyIncomeCalc: function() {
+    return (Session.get("editingMonthlyIncomeCalc"));
+  },
+  editingCAPRateCalc: function() {
+    return (Session.get("editingCAPRateCalc"));
   }
+
+
 });
+
+
 
 Template.calculator.events({
-  'change #purchasePrice': function(e,t) {
-
+  'click #purchase-price-div': function(e,t) {
+    Session.set("editingTotalPriceCalc", true);
   },
-  'change #rehabCost': function(e,t) {
-
+  'click #closing-repair-div': function(e,t) {
+    Session.set("editingClosingRepairCalc", true);
   },
-  'change #monthlyIncome': function(e,t) {
-
+  'click #monthly-income-div': function(e,t) {
+    Session.set("editingMonthlyIncomeCalc", true);
   },
-  'change #capRate': function(e,t) {
+  'click #cap-rate-div': function(e,t) {
+    Session.set("editingCAPRateCalc", true);
+  },
 
+
+  'keypress input#purchase-price': function(e,t) {
+    if (e.keyCode === 13) {
+      Session.set("editingTotalPriceCalc", false);
+      Calculator.update({ _id: this._id}, { $set: { purchasePrice: e.currentTarget.value }});
+    }
+  },
+  'keypress input#closing-repair': function(e,t) {
+    if (e.keyCode === 13) {
+      Session.set("editingClosingRepairCalc", false);
+      Calculator.update({ _id: this._id}, { $set: { closingRepair: e.currentTarget.value }});
+    }
+  },
+  'keypress input#monthly-income': function(e,t) {
+    if (e.keyCode === 13) {
+      Session.set("editingMonthlyIncomeCalc", false);
+      Calculator.update({ _id: this._id}, { $set: { monthlyIncome: e.currentTarget.value }});
+    }
+  },
+  'keypress input#cap-rate': function(e,t) {
+    if (e.keyCode === 13) {
+      Session.set("editingCAPRateCalc", false);
+      Calculator.update({ _id: this._id}, { $set: { capRate: e.currentTarget.value }});
+    }
   }
+
+
+
 });
+
+
+// 'click #total-price-div': function(e,t) { Session.set("editingTotalPrice", true); },
