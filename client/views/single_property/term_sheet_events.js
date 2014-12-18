fa742a37d81700caa9ccd8ac2cf576043534eddc
Sam Corcos <samcorcos@gmail.com>
2014-12-17 19:31:28 -0500

    added click and helpers for all term sheet buttons

--- client/views/single_property/term_sheet_events.js
+++ client/views/single_property/term_sheet_events.js
@@ -1,12 +1,21 @@
 Template.singlePropertyTermSheet.events({
-  'click #total-price-div': function(e,t) {
-    console.log(this._id)
-  },
   'click #add-term-sheet-button': function(e,t) {
     $(".flex-white-div-term-sheet").toggleClass("add-flex-div-show");
     $(".red-break-term-sheet").toggleClass("add-red-break-show");
     TermSheet.insert({ "owner": Meteor.userId(), "property": window.location.href.match(/(\/properties\/)(\w+)/)[2], "totalPrice": 0, "downPayment": 0, "closingRepair": 0, "apr": 0, "taxes": 0, "hoa": 0, "insurance": 0, "rentPrice": 0, "squareFootage": 0 });
     $(".flex-white-div-term-sheet-button").toggleClass("add-flex-div-hide");
     $(".red-break-term-sheet-button").toggleClass("add-red-break-hide");
-  }
+  },
+
+
+  'click #total-price-div': function(e,t) { Session.set("editingTotalPrice", true); },
+  'click #down-payment-div': function(e,t) { Session.set("editingDownPayment", true); },
+  'click #closing-repair-div': function(e,t) { Session.set("editingClosingRepair", true); },
+  'click #apr-div': function(e,t) { Session.set("editingAPR", true); },
+  'click #taxes-div': function(e,t) { Session.set("editingTaxes", true); },
+  'click #hoa-div': function(e,t) { Session.set("editingHOA", true); },
+  'click #insurance-div': function(e,t) { Session.set("editingInsurance", true); },
+  'click #rent-price-div': function(e,t) { Session.set("editingRentPrice", true); },
+  'click #square-footage-div': function(e,t) { Session.set("editingSquareFootage", true); }
+
 });
--- client/views/single_property/term_sheet_helpers.js
+++ client/views/single_property/term_sheet_helpers.js
@@ -4,5 +4,16 @@ Template.singlePropertyTermSheet.helpers({
   },
   termSheetActive: function() {
     return Session.get("termSheetActive");
-  }
+  },
+
+  editingTotalPrice: function() { return Session.get("editingTotalPrice"); },
+  editingDownPayment: function() { return Session.get("editingDownPayment"); },
+  editingClosingRepair: function() { return Session.get("editingClosingRepair"); },
+  editingAPR: function() { return Session.get("editingAPR"); },
+  editingTaxes: function() { return Session.get("editingTaxes"); },
+  editingHOA: function() { return Session.get("editingHOA"); },
+  editingInsurance: function() { return Session.get("editingInsurance"); },
+  editingRentPrice: function() { return Session.get("editingRentPrice"); },
+  editingSquareFootage: function() { return Session.get("editingSquareFootage"); }
+
 });
