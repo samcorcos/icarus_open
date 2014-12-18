1d45847eb027411827dee4b0c9379ff59ee10855
Sam Corcos <samcorcos@gmail.com>
2014-12-18 01:34:13 -0500

    refactoring helpers

--- client/views/single_property/term_sheet_helpers.js
+++ client/views/single_property/term_sheet_helpers.js
@@ -25,6 +25,7 @@ Template.termSheetInputs.helpers({
 
 });
 
+
 Template.termSheetOutputs.helpers({
   termSheet: function() {
     return TermSheet.find({ property: Session.get("currentId")._id });
@@ -33,21 +34,54 @@ Template.termSheetOutputs.helpers({
 
   downPaymentAmount: function() {
     var temp = TermSheet.find({ property: Session.get("currentId")._id }).fetch();
-    return (temp[0].totalPrice * temp[0].downPayment / 100).formatMoney(0);
+    var downPaymentAmount = (Number(temp[0].totalPrice) * Number(temp[0].downPayment) / 100);
+    return downPaymentAmount.formatMoney(0);
   },
   additionalFinancing: function() {
-    return 0;
+    return 0;                                   // Has not been filled in
   },
   totalInvestment: function() {
     var temp = TermSheet.find({ property: Session.get("currentId")._id }).fetch();
-    return (Number(temp[0].totalPrice) + Number(temp[0].closingRepair)).formatMoney(0);
+    var totalInvestment = (Number(temp[0].totalPrice) + Number(temp[0].closingRepair))
+    return totalInvestment.formatMoney(0);
   },
   monthlyMortgageExpense: function() {
-    return 0;
+    return 0;                                 // Has not been filled in
   },
   monthlyTaxAverage: function() {
     var temp = TermSheet.find({ property: Session.get("currentId")._id }).fetch();
-    return (temp[0].taxes / 12).formatMoney(0);
+    var monthlyTaxAverage = (temp[0].taxes / 12);
+    return monthlyTaxAverage.formatMoney(0);
+  },
+  operatingExpenses: function() {
+    var temp = TermSheet.find({ property: Session.get("currentId")._id }).fetch();
+    var operatingExpenses = (Number((temp[0].taxes / 12)) + Number(temp[0].hoa) + Number(temp[0].insurance) + Number((temp[0].rentPrice) * 0.05));
+    return operatingExpenses.formatMoney(0);
+  },
+  reserve: function() {
+    var temp = TermSheet.find({ property: Session.get("currentId")._id }).fetch();
+    var reserve = (temp[0].rentPrice * 0.05);
+    return reserve.formatMoney(0);
+  },
+  monthlyCostOfOwnership: function() {
+    var temp = TermSheet.find({ property: Session.get("currentId")._id }).fetch();
+    var monthlyCostOfOwnership = (Number((temp[0].taxes / 12)) + Number(temp[0].hoa) + Number(temp[0].insurance) + Number((temp[0].rentPrice) * 0.05)); // Does not include monthly mortgage expense, or cost of additional financing
+    return monthlyCostOfOwnership.formatMoney(0);
+  },
+  cashflowRented: function() {
+    var temp = TermSheet.find({ property: Session.get("currentId")._id }).fetch();
+    var cashflowRented = (Number(temp[0].rentPrice) - (Number((temp[0].taxes / 12)) + Number(temp[0].hoa) + Number(temp[0].insurance) + Number((temp[0].rentPrice) * 0.05))); // Does not include monthly mortgage expense, or cost of additional financing
+    return cashflowRented.formatMoney(0);
+  },
+  cashflowOccupied: function() {
+    var temp = TermSheet.find({ property: Session.get("currentId")._id }).fetch();
+    var cashflowOccupied = (Number(temp[0].rentPrice) - (Number((temp[0].taxes / 12)) + Number(temp[0].hoa) + Number(temp[0].insurance) + Number((temp[0].rentPrice) * 0.05))); // Does not include monthly mortgage expense, or cost of additional financing
+    return cashflowOccupied.formatMoney(0);
+  },
+  cashflowUnoccupied: function() {
+    var temp = TermSheet.find({ property: Session.get("currentId")._id }).fetch();
+    var cashflowUnoccupied = (-1 * (Number((temp[0].taxes / 12)) + Number(temp[0].hoa) + Number(temp[0].insurance) + Number((temp[0].rentPrice) * 0.05))); // Does not include monthly mortgage expense, or cost of additional financing
+    return cashflowUnoccupied.formatMoney(0);
   }
 
 
