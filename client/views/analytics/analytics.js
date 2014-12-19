5ae7380f3f3dacf2377a57a4aebc5ede260935d0
Sam Corcos <samcorcos@gmail.com>
2014-12-18 18:14:56 -0600

    notes on analytics

--- client/views/analytics/analytics.js
+++ client/views/analytics/analytics.js
@@ -3,24 +3,23 @@ Template.analytics.rendered = function() {
   createAssetAllocation();
   $('.tooltipped').tooltip({"delay": 50});
 
-//   Meteor.call("getTweets", "javascript", function(err, tweets) { // in this case, we are running the "getTweets" method, then passing in the search term "javascript", then a callback
-//   Session.set("tweets", tweets);
-//   var d = new Date();
-//   Session.set("latestRefresh", "" + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()); // In this case, we are starting off with an empty string because d.getHours() returns a number, not a string, so we wouldn't be able to concatenate the strings with numbers if we didnt initialize it as a string.
-// })
-
-
   Meteor.call("getProperty", "63706372", function(err, zest) {
     if (err) { console.log("error") }
 
     Session.set("zestimate", zest);
-    console.log("success")
+    console.log("success");
 
   })
 };
 
 Template.returnOnInvestment.helpers({
-  zestimate: function() { return Session.get("zestimate"); }
+  zestimate: function() {
+    var temp = Session.get("zestimate");
+    return Number(temp).formatMoney(0);
+  }
+
+// Go through all term sheets you own, compare prices paid to API call, ???, Profit
+
 
 });
 
--- server/analytics.js
+++ server/analytics.js
@@ -1,17 +1,7 @@
 Meteor.methods({
   getProperty: function(zpid) {
     var response = Meteor.http.call("GET", "http://www.zillow.com/webservice/GetZestimate.htm?zws-id=X1-ZWz1e01y8ugd8r_1brbp&zpid="+zpid);
-    // var zestimate = response.match(/(<zestimate><amount currency="USD">)(\d+)/[2]);
-    // console.log(response.content.match(/(<zestimate><amount currency="USD">)(\d+)/)[2])
-    console.log("success");
     return response.content.match(/(<zestimate><amount currency="USD">)(\d+)/)[2];
-
-    // return response.data.results.map(function (tweet) {
-    //   return {
-    //     user: tweet.from_user_name,
-    //     text: tweet.text
-    //   }
-    // })
   }
 })
 
