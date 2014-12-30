7022b00298a8ef9551a4538a632e003630d2b207
Sam Corcos <samcorcos@gmail.com>
2014-12-29 20:56:57 -0800

    adding analytics helpers

--- client/views/analytics/analytics.js
+++ client/views/analytics/analytics.js
@@ -10,13 +10,45 @@ Template.analytics.rendered = function() {
 
 Template.returnOnInvestment.helpers({
   totalAssetAppreciationPercent: function() {
+    var total = 0;
+    var zest = 0;
 
+    Properties.find().fetch().forEach(function(property) {
+      var currentId = property._id;
+      var zestimate = Number(property.zestimate);
+
+      var temp = TermSheet.find({ property: currentId}).fetch()[0];
+
+      var closingRepair = Number(temp.closingRepair);
+      var totalPrice = Number(temp.totalPrice);
+      var price = closingRepair + totalPrice;
+
+      zest += zestimate;
+      total += price;
+    })
+
+    var percent = zest / total * 100;
+
+    return percent.formatMoney(2);
   },
   averageMonthlyRentalReturns: function() {
 
   },
   totalAssetAppreciation: function() {
+    var total = 0;
 
+    Properties.find().fetch().forEach(function(property) {
+      var currentId = property._id;
+      var zestimate = Number(property.zestimate);
+
+      var temp = TermSheet.find({ property: currentId}).fetch()[0];
+
+      var closingRepair = Number(temp.closingRepair);
+      var totalPrice = Number(temp.totalPrice);
+      var price = closingRepair + totalPrice;
+      total += (zestimate - price);
+    })
+    return total.formatMoney(0);
   },
   averageMonthlyLoanReturns: function() {
 
